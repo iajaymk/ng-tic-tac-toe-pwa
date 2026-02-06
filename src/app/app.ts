d028d1745c12642c3292fde561ac43a67034634a
Ajay Kumar <ajaykumarmkaju@gmail.com>
2026-02-06 23:53:06 +0530

    Added basic ui and working for tic tac toe game

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Board } from './board/board';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Board],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected title = 'tic-tac-toe';
